7f6920b0b62e2ce6e472daed64d94940395b08c4
Paweł Wiaderny <zaak@users.noreply.github.com>
2023-09-23 08:19:19 +0200

    feat: application boilerplate

---
+++ resources/views/ckbox-example-layout.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8"/>
+        <script src="https://cdn.ckbox.io/ckbox/{@var ckbox-version}/ckbox.js"></script>
+        <title>CKBox example</title>
+    </head>
+    <body>
+    @yield('content')
+    @yield('scripts')
+    </body>
+</html>
